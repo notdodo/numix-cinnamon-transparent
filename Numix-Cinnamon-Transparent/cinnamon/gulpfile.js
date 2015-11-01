f75c68a875652fd3d6489119fdc78d1bebf22981
Edoardo Rosa <edoardo.rosa90@gmail.com>
2015-11-01 23:29:47 +0100

    new version w/ sass and gulp

--- Numix-Cinnamon-Transparent/cinnamon/gulpfile.js
+++ Numix-Cinnamon-Transparent/cinnamon/gulpfile.js
@@ -47,49 +47,7 @@ gulp.task("reloadTheme", ["sass"], shell.task([
     "gsettings set org.cinnamon.theme name "+theme
 ]));
 
-
-// Make a symlink in the ~/.themes dir
-gulp.task("install", function () {
-
-    // Ensure that ".themes" dir exists
-    try {
-        fs.mkdirSync(themesDir);
-    } catch (err) {
-        if (err.code !== "EEXIST") throw err;
-    }
-
-
-    // Check whether the theme dir/file exists in ".themes"
-    // If it does exist, remove it
-    try {
-        if( fs.lstatSync(themesDir+theme).isDirectory() ||
-            fs.lstatSync(themesDir+theme).isFile() ) {
-
-            rimraf.sync(themesDir+theme);
-        }
-    } catch (err) {
-        if (err.code !== "ENOENT") throw err;
-    }
-
-
-    // Create/update the link
-    try {
-        fs.unlinkSync(themesDir+theme);
-    } catch (err) {
-        if (err.code !== "ENOENT") throw err;
-    }
-
-    fs.symlinkSync(__dirname+"/../../"+theme, themesDir+theme);
-});
-
-
-// Watch
-gulp.task("watch", function () {
-    gulp.watch(["sass/**/*"], ["reloadTheme"]);
-});
-
-
 // Default task
-gulp.task("default", ["install", "watch"], function() {
+gulp.task("default", function() {
     gulp.start("reloadTheme");
 });
